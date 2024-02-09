f04ac3a121d94d857c4b1e6e2231c3500adb0bdd
Jintaozyexpress <154585525+Jintaozyexpress@users.noreply.github.com>
2024-02-09 19:29:19 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = ["新年快乐", "心想事成"];
+const randomWords = ["龙年快乐", "心想事成","龙行龘龘","前程朤朤","生活䲜䲜","事业骎骎"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
 	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
